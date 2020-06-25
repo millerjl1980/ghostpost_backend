15e0a83dc0e7679319074897f965762ae225f4d4
Justin Miller <indymiller@gmail.com>
2020-06-24 22:16:04 -0400

    updated back end to display boast or roast title

--- api/serializers.py
+++ api/serializers.py
@@ -1,8 +1,11 @@
-from rest_framework.serializers import ModelSerializer
+from rest_framework import serializers
+from rest_framework.serializers import ModelSerializer, StringRelatedField
 
 from ghostpost.models import Post
 
+# https://stackoverflow.com/questions/28945327/django-rest-framework-with-choicefield
 class PostSerializer(ModelSerializer):
+    post_type = serializers.CharField(source='get_post_type_display')
     class Meta:
         model = Post
         fields = (
